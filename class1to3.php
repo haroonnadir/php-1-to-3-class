1eea116900de20f35ee30aa57ebb6fd99037b022
Haroon <biodunhi@gmail.com>
2024-06-12 13:55:10 +0500

    first commit

---
+++ class1to3.php
@@ -0,0 +1,25 @@
+<?php
+echo"halo my name is noor <br>";
+
+
+// <!-- veriable -->
+ $x = 20;
+ $y = 40;
+ $z = $x+$y;
+echo"<br>";
+ echo"$z";
+
+
+
+ $x = "H@Roon N@diR";
+ $x = 2500;
+ $x = 2500.50;
+ $x = true;
+ $x = array("html","css","js");
+ $x = null;
+ echo  $x . "<br>";
+ echo  $x[0] . "<br>";
+ var_dump($x);
+
+
+ ?>
